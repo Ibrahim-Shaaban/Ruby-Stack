0808def0d7ebed93294fde431039d12db878aa71
Ibrahim Shaaban <hemaabdelnaby325@gmail.com>
2022-08-10 11:39:26 +0200

    update answers

--- run.rb
+++ run.rb
@@ -45,14 +45,23 @@ puts "average is #{e.mean}"
 
 ################### answer questions ######################
 # I think this soultion is fast as : 
-# - max method : 
-# * required only one step to return the max element with time complexity O(1) as it is calculated for each push to stack to compare # # the new element with with the old max element and so updated 
-# - push method : 
-# * time complexity O(1) and we do some logic to make it easy when we get the max , sum , and length 
-# * each push we update the length , the sum , and the max element to make the max and mean methods very fast
-# - pop method : 
-# * I think that my solution for it may be not the best but i tried to return all elements with number of steps equal to half of # # # # elements length with out removing any elelment from stack as descriped in the task file
-# - mean method : 
-# * as the max method it requires one step to be calculated as each push updted the sum of all elements nad the length of elements so # # we calculated average by dividing sum to length
+
+# max method : 
+# * requires only one step to return the max element with time complexity O(1) as it is calculated for each push to stack to compare
+# the new element with with the old max element and so updated 
+
+
+# push method : 
+# * time complexity O(1) and we do some logic to make it easy when we get the max , sum  and length 
+# * each push we update the length , the sum and the max element to make the max and mean methods very fast
+
+
+# pop method : 
+# * I think that my solution for it may be not the best but i tried to return all elements with number of steps equal to half of 
+# elements length with out removing any elelment from stack as descriped in the task file
+
+
+# mean method : 
+# * as the max method it requires one step to be calculated as each push updated the sum of all elements and the length of elements so # we calculated average by dividing sum to length
 # * time complexity for it is O(1) 
 ###########################################################
